b6d2733a10ff81ca4d01f0f5d99fe59d18aac599
M01-M01 <marlonsaddress@gmail.com>
2018-06-18 18:39:57 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  languages_hash = {}
+  languages.each do | paradigm, lang_list |
+    lang_list.each do | language, type_hash |
+      type_hash.each do |header, type|
+        shovel_data = {header => type, :style => []}
+        languages_hash[language] = shovel_data
+      end
+    end
+  end
+  languages.each do | paradigm, lang_list |
+    languages_hash.each do | language, data |
+      if lang_list.has_key?(language)
+          data[:style] << paradigm
+      end
+    end
+  end
+  return languages_hash
 end
