718de37fffbd76485194b60536de4e2023e1cd77
Dan Chung <danetix@gmail.com>
2018-06-20 13:15:16 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |style, language|
+    language.each do |name, data|
+      if new_hash[name].nil? # Check if language has already been added
+        new_hash[name] = {
+          type: data[:type],
+          style: [style]
+        }
+      else # If language exists in hash, push style into array
+        new_hash[name][:style] << style
+      end
+    end
+  end
+
+  new_hash
 end
