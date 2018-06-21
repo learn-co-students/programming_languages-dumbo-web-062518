373f09a7c21430e2f0de8711fcbe652d8f3b8d0e
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 23:36:22 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, lang|
+    lang.each do |name, style|
+      if new_hash.has_key?(name)
+        new_hash[name][:style].push(type)
+      else
+        new_hash[name] = style
+        new_hash[name][:style] = [type]
+      end
+    end
+  end
+  new_hash
 end
